b3affae338df32ce75b31adf6bf1262ec4299a3e
Dan Leehr <dan.leehr@duke.edu>
2015-08-14 13:18:26 -0400

    Move python code below python dir

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Render hub
-python render/render.py yaml/hub/hub.yaml hub
+python python/render/render.py yaml/hub/hub.yaml hub
 
 # Render genomes
-python render/render.py yaml/genomes/genomes.yaml genomes
+python python/render/render.py yaml/genomes/genomes.yaml genomes
 
 # Render trackDbs
-python render/render.py yaml/tracks/hg19.yaml trackDb
-python render/render.py yaml/tracks/hg38.yaml trackDb
+python python/render/render.py yaml/tracks/hg19.yaml trackDb
+python python/render/render.py yaml/tracks/hg38.yaml trackDb
