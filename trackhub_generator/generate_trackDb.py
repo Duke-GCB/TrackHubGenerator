bd0bad78fdb381c8946a6d2bc57750d00b4a6e2e
Dan Leehr <dan.leehr@duke.edu>
2015-08-14 11:39:24 -0400

    Use argparse

--- trackhub_generator/generate_trackDb.py
+++ trackhub_generator/generate_trackDb.py
@@ -1,13 +1,25 @@
-import sys
 import yaml
+import argparse
+
 from jinja2 import Environment, PackageLoader
 
-def render_trackdb(track_dicts):
-    env = Environment(loader=PackageLoader('trackhub_generator', 'templates'))
-    template = env.get_template('trackDb.j2')
-    print template.render(track_dicts)
+
+def render_template(yaml_object, template_name):
+    env = Environment(loader=PackageLoader(package_name='trackhub_generator'))
+    template = env.get_template(template_name + '.j2')
+    print template.render(yaml_object)
 
 
-with open(sys.argv[1]) as tracks_yaml:
-    tracks = yaml.load(tracks_yaml)
-    render_trackdb(tracks)
+def main(yaml_file_name, template_name):
+    with open(yaml_file_name) as yaml_file:
+        obj = yaml.load(yaml_file)
+        render_template(obj,template_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml_file', type=argparse.FileType('r'))
+    parser.add_argument('template_name')
+    parser.add_argument_group()
+    args = parser.parse_args()
+    main(args.yaml_file.name, args.template_name)
