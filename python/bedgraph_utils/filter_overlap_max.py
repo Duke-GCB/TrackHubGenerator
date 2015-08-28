f12f0326bf9014a2a47c71e339715ed8113aeec4
Dan Leehr <dan.leehr@duke.edu>
2015-08-28 15:47:16 -0400

    Adjust processing to collapse adjacent regions with same value
    But now produces overlapping regions

--- python/bedgraph_utils/filter_overlap_max.py
+++ python/bedgraph_utils/filter_overlap_max.py
@@ -28,13 +28,37 @@ def rows_covering_base(rows, base):
             yield row
 
 
-def process_region(region):
+def collapse_region(region):
+    chrom, start, end, value = None, None, None, None
+    for r in region:
+        value = value or r[COL_VALUE]
+        chrom = chrom or r[COL_CHROM]
+        start = start or r[COL_START]
+        end = end or r[COL_END]
+        if value == r[COL_VALUE]:
+            end = r[COL_END]
+        else:
+            # Clear out old region, begin new
+            yield (chrom, start, end, value)
+            # new value, new region
+            value = r[COL_VALUE]
+            start = r[COL_START]
+            end = r[COL_END]
+    yield (chrom, start, end, value)
+
+
+def expand_region(region):
     chrom = region[0][COL_CHROM]
     start = min([x[COL_START] for x in region])
     end = max([x[COL_END] for x in region])
     for base in range(start, end):
         value = max([x[COL_VALUE] for x in rows_covering_base(region, base)])
         yield (chrom, base, base + 1, value)
+
+
+def process_region(region):
+    for c in collapse_region(expand_region(region)):
+        yield c
 
 
 def filter_overlap_max(input_file, output_file):
