5e152c1c6eefedc2bd7b5e833fc7927a6abb659e
Dan Leehr <dan.leehr@duke.edu>
2015-09-03 11:29:59 -0400

    Python script to scale values in bedgraph file

---
+++ python/bedgraph_utils/scale.py
@@ -0,0 +1,22 @@
+from bedgraph_utils import *
+import sys
+import fileinput
+import csv
+
+
+def change_bedgraph_score_scale(input, output, factor=.01):
+    """
+    Modifies bedgraph data by multiplying by a scale factor
+    :param input: An input stream or open file
+    :param output: An output stream
+    :param factor: Value by which to multiply COL_VALUE
+    :return:
+    """
+    reader = csv.reader(input, delimiter='\t')
+    writer = csv.writer(output, delimiter='\t')
+    for row in reader:
+        row[COL_VALUE] = float(row[COL_VALUE]) * factor
+        writer.writerow(row)
+
+if __name__ == '__main__':
+    change_bedgraph_score_scale(fileinput.input(mode='rb'), sys.stdout)
