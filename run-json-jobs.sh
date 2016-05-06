6b0836ba4ab1138f52209675df193c923b592f72
Dan Leehr <dan.leehr@duke.edu>
2016-05-06 16:07:34 -0400

    Add outdir to cwltool command, and remove exit after first job

--- run-json-jobs.sh
+++ run-json-jobs.sh
@@ -2,8 +2,7 @@
 set -e
 JSON_DIR=$1
 CWL_PATH=`pwd`/cwl/bin # Must be absolute
-
+OUTDIR=$2
 for f in $JSON_DIR/*.json; do
-  PATH=$PATH:$CWL_PATH cwltool cwl/bigbed-workflow.cwl $f
-  exit
+  PATH=$PATH:$CWL_PATH cwltool --outdir $OUTDIR cwl/bigbed-workflow.cwl $f
 done
