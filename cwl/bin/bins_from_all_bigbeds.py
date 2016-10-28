a3aa95d0ca471b5596ca5853115003fd77225150
Dan Leehr <dan.leehr@duke.edu>
2016-10-28 14:56:59 -0400

    Reworks bins_from_bedfile to handle non-unique results from qcut
    Adds some additional arguments to bins_from_all_bigbeds

--- cwl/bin/bins_from_all_bigbeds.py
+++ cwl/bin/bins_from_all_bigbeds.py
@@ -10,7 +10,7 @@ BIGBED_TO_BED_BIN='/Users/dcl9/bin/bigBedToBed'
 def make_bedfile_name(bigbed_filename):
     return bigbed_filename.replace('.bb','.bed')
 
-def main(bigbeds_dir):
+def main(bigbeds_dir, output_file):
     results = list()
     bigbed_filenames = glob.glob(os.path.join(bigbeds_dir, '*.bb'))
     for bigbed in bigbed_filenames:
@@ -20,13 +20,16 @@ def main(bigbeds_dir):
         pos_bins, neg_bins = bins_from_bed(bed, 9, 9)
         result = format_for_imads_config(os.path.basename(bed), pos_bins, neg_bins)
         results.append(result)
+        print yaml.dump(result)
         # delete the file
         os.remove(bed)
-    print yaml.dump(results)
+    with open(output_file, 'w') as f:
+        print yaml.dump(results, f)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('bigbeds_dir', help='directory of bigbed files')
+    parser.add_argument('--output_file', default='bins.yaml')
     args = parser.parse_args()
-    main(args.bigbeds_dir)
+    main(args.bigbeds_dir, args.output_file)
--- cwl/bin/bins_from_bedfile.py
+++ cwl/bin/bins_from_bedfile.py
@@ -4,14 +4,28 @@ import argparse
 import os
 import yaml
 
+def bins_from_data(data, n_bins):
+    try:
+        bins = pandas.qcut(data, n_bins, retbins=True)[1].tolist()
+        return bins
+    except:
+        # If we can't cut into n_bins without error, cut into n_bins-1, and then divide the largest bin
+        bins = bins_from_data(data, n_bins - 1)
+        while len(bins) < n_bins:
+            widths = [abs(bins[i] - bins[i + 1]) for i in range(len(bins) - 1)]
+            largest_index = widths.index(max(widths))
+            to_insert = (bins[largest_index] + bins[largest_index + 1]) / 2.0
+            bins.insert(largest_index + 1, to_insert)
+        return bins
+
 def bins_from_bed(bedfile, n_posbins, n_negbins):
     data = pandas.read_table(bedfile, delimiter='\t', header=None)
     score_column = 3
     pos_scores = data[data[score_column] > 0][3]
     neg_scores = data[data[score_column] < 0][3]
-    pos_bins = pandas.qcut(pos_scores, n_posbins, retbins=True)[1]
-    neg_bins = pandas.qcut(neg_scores, n_negbins, retbins=True)[1]
-    return pos_bins.tolist(), neg_bins.tolist()
+    pos_bins = bins_from_data(pos_scores, n_posbins)
+    neg_bins = bins_from_data(neg_scores, n_negbins)
+    return pos_bins, neg_bins
 
 def format_for_imads_config(filename, pos_bins, neg_bins):
     # hg19_E2f1_0007_vs_E2f3_0008.bed
